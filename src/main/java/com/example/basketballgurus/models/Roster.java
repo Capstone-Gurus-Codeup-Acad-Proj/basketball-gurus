6b4034b63a63605bdd7e45563ce17b2a2d75d872
Allison Falls <allison.falls2@gmail.com>
2021-10-08 16:18:02 -0400

    roster

--- src/main/java/com/example/basketballgurus/models/Roster.java
+++ src/main/java/com/example/basketballgurus/models/Roster.java
@@ -1,4 +1,56 @@
 package com.example.basketballgurus.models;
 
+import java.util.Date;
+
 public class Roster {
+
+    private int id;
+
+    private int roster_id;
+
+    private String team;
+
+    private int team_id;
+
+    private int user_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoster_id() {
+        return roster_id;
+    }
+
+    public void setRoster_id(int roster_id) {
+        this.roster_id = roster_id;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
 }
