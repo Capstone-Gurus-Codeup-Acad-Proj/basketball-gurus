962511132dccb0aa99a4b17c222dd0b4b8b139ac
Seth Gardner <seth.gardner.a@gmail.com>
2021-10-12 10:03:57 -0500

    added lombak

--- src/main/java/com/example/basketballgurus/models/User.java
+++ src/main/java/com/example/basketballgurus/models/User.java
@@ -1,63 +1,53 @@
 package com.example.basketballgurus.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private int id;
+
     @Column(nullable = false, length = 100)
+    @Getter @Setter
     private String userName;
+
     @Column(nullable = false, length = 100)
+    @Getter @Setter
     private String firstName;
+
     @Column(nullable = false, length = 100)
+    @Getter @Setter
     private String lastName;
+
     @Column(nullable = false, length = 100)
+    @Getter @Setter
     private String email;
+
     @Column(nullable = false, length = 100)
+    @Getter @Setter
     private String password;
-    @Column()
+
+    @Column
+    @Getter @Setter
     private String profilePicture;
-    @Column()
+
+    @Column
+    @Getter @Setter
     private String banner;
-    @Column()
+
+    @Column
+    @Getter @Setter
     private String bio;
     @Column(nullable = false)
     private boolean isActive;
-    public User() {
-    }
-    public User(String userName, String firstName, String lastName, String email, String password, String profilePicture, String banner, String bio, int id, boolean isActive) {
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.profilePicture = profilePicture;
-        this.banner = banner;
-        this.bio = bio;
-        this.id = id;
-        this.isActive = isActive;
-    }
-    public String getUserName() {return userName;}
-    public void setUserName(String userName) {this.userName = userName;}
-    public String getFirstName() {return firstName;}
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public String getLastName() {return lastName;}
-    public void setLastName(String lastName) {this.lastName = lastName;}
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
-    public String getProfilePicture() {return profilePicture;}
-    public void setProfilePicture(String profilePicture) {this.profilePicture = profilePicture;}
-    public String getBanner() {return banner;}
-    public void setBanner(String banner) {this.banner = banner;}
-    public String getBio() {return bio;}
-    public void setBio(String bio) {this.bio = bio;}
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
-    public boolean isActive() {return isActive;}
-    public void setActive(boolean active) {isActive = active;}
+
+
 }
