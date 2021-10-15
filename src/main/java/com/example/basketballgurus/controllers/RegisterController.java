4e928e1ab1f228fccc8b9ff5ad86052b1a4b89e6
Allison Falls <allison.falls2@gmail.com>
2021-10-15 11:57:03 -0400

    register finished

--- src/main/java/com/example/basketballgurus/controllers/RegisterController.java
+++ src/main/java/com/example/basketballgurus/controllers/RegisterController.java
@@ -1,22 +1,29 @@
 package com.example.basketballgurus.controllers;
 
 import com.example.basketballgurus.models.User;
+import com.example.basketballgurus.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterController {
+    private final UserRepository userDao;
+
+    public RegisterController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping("/register")
     public String showCreateForm(Model model) {
         model.addAttribute("user", new User());
-        return "user";
+        return "user/register";
     }
     @PostMapping("/register")
     public String create(@ModelAttribute User user) {
-        return "/profile";
+        userDao.save(user);
+        return "redirect:/";
     }
 }
