4b9ab0c3db5be5fcb0e097e50a89a066b1801cb5
Allison Falls <allison.falls2@gmail.com>
2021-10-21 16:57:55 -0400

    security pass map

--- src/main/java/com/example/basketballgurus/controllers/RegisterController.java
+++ src/main/java/com/example/basketballgurus/controllers/RegisterController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class RegisterController {
     private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public RegisterController(UserRepository userDao) {
+    public RegisterController(UserRepository userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register")
@@ -27,6 +29,8 @@ public class RegisterController {
 
     @PostMapping("/register")
     public String create(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
         userDao.save(user);
         return "user/login";
     }
