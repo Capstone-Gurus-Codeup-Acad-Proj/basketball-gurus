b131230d8da48ea9ecade20a02e32830f93beb18
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-10-21 12:49:06 -0500

    "weeks games"

--- src/main/java/com/example/basketballgurus/controllers/RegisterController.java
+++ src/main/java/com/example/basketballgurus/controllers/RegisterController.java
@@ -2,6 +2,7 @@ package com.example.basketballgurus.controllers;
 
 import com.example.basketballgurus.models.User;
 import com.example.basketballgurus.repos.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,19 +12,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class RegisterController {
     private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public RegisterController(UserRepository userDao) {
+    public RegisterController(UserRepository userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
+
 
     @GetMapping("/register")
     public String showCreateForm(Model model) {
         model.addAttribute("user", new User());
         return "user/register";
     }
+    //added saving user /hash pass
     @PostMapping("/register")
     public String create(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
         userDao.save(user);
-        return "redirect:/";
+        return "redirect:/login";
     }
+
+
 }
