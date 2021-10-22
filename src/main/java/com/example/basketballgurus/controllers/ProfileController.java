9eccd25268e4242992ed785836435951549d03db
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-10-22 10:20:17 -0500

    "profile update"

--- src/main/java/com/example/basketballgurus/controllers/ProfileController.java
+++ src/main/java/com/example/basketballgurus/controllers/ProfileController.java
@@ -2,24 +2,22 @@ package com.example.basketballgurus.controllers;
 
 import com.example.basketballgurus.models.User;
 import com.example.basketballgurus.repositories.UserRepository;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.example.basketballgurus.services.GameBarService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.model.IModel;
-
-import java.util.Optional;
 
 @Controller
 public class ProfileController {
     private final UserRepository userDao;
     private final PasswordEncoder passwordEncoder;
+    private final GameBarService gm;
 
-    public ProfileController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+    public ProfileController(UserRepository userDao, PasswordEncoder passwordEncoder, GameBarService gm) {
         this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
+        this.gm = gm;
     }
 
 //    @GetMapping("/profile")
@@ -31,6 +29,7 @@ public class ProfileController {
     public String userProfile(Model model) {
  //       User currentUser = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.getById(1L);
+        model.addAttribute("games", gm.getTodaysGames());
         model.addAttribute("user",user);
 //        if (!user.isActive()) {
 //            return "redirect:/home";
