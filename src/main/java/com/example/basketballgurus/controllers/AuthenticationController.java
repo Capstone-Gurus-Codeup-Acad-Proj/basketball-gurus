d6eb1fedbc637a1e88985e8af787c1518adb9984
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-10-22 07:18:21 -0500

    minor updates

--- src/main/java/com/example/basketballgurus/controllers/AuthenticationController.java
+++ src/main/java/com/example/basketballgurus/controllers/AuthenticationController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
-        return "login1";
+        return "user/login";
     }
 }
