d2f6671ed3fd8e43437dfce94de58b57528046b5
Seth Gardner <seth.gardner.a@gmail.com>
2021-10-21 23:06:06 -0500

    finished basics of schedule bar

--- src/main/java/com/example/basketballgurus/controllers/LandingPageController.java
+++ src/main/java/com/example/basketballgurus/controllers/LandingPageController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -25,12 +27,13 @@ public class LandingPageController {
     public String landingPage(Model model) {
         List<Game> allGames = gameDao.findAll();
         ArrayList<Game> fiveGames = new ArrayList<>();
+        LocalDate date = LocalDate.now();
+        String today = Integer.toString(date.getDayOfMonth());
         for(Game game : allGames){
-            if (fiveGames.size() == 10){
-                break;
-            }
-            if (!game.getFinished()){
-
+            String pattern = "d";
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            String gameDate =  sdf.format(game.getStartTime());
+            if (today.equals(gameDate)){
                 fiveGames.add(game);
             }
         }
