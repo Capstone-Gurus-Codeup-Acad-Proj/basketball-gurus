346d619f5a922915fd440ae8533e9332c51584e4
Allison Falls <allison.falls2@gmail.com>
2021-10-25 16:07:39 -0400

    users can view by team

--- src/main/java/com/example/basketballgurus/repositories/TeamRepository.java
+++ src/main/java/com/example/basketballgurus/repositories/TeamRepository.java
@@ -2,8 +2,10 @@ package com.example.basketballgurus.repositories;
 
 import com.example.basketballgurus.models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+    Team findTeamByFullName(String name);
 }
