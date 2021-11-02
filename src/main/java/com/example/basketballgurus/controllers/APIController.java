bf61b31fce3255156cc5f26612c94dff61f675ff
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-11-02 16:33:04 -0500

    minor changes to API Controller

--- src/main/java/com/example/basketballgurus/controllers/APIController.java
+++ src/main/java/com/example/basketballgurus/controllers/APIController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.MessageFormat;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class APIController {
@@ -15,8 +16,6 @@ public class APIController {
     @ResponseBody
     public String apiKey() {
         System.out.println(apiKey);
-        final AtomicReference<String> format = new AtomicReference<>(String.format("const apiKey = `Isnull", apiKey));
-        return format.get();
-
+        return MessageFormat.format("const ApiKey = `{0}null", apiKey);
     }
 }
