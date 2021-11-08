d0ef26a4d0b095737493a57d4935feabbe6ee8ca
Allison Falls <allison.falls2@gmail.com>
2021-11-08 14:39:37 -0500

    included timestamp

--- src/main/java/com/example/basketballgurus/SecurityConfiguration.java
+++ src/main/java/com/example/basketballgurus/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/home") // user's home page, it can be any URL
+                .defaultSuccessUrl("/") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
@@ -49,7 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/home") // anyone can see the home and the ads pages
+                .antMatchers( "/") // anyone can see the home and the ads pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
