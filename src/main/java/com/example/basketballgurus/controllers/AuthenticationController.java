79b41fdbe2f8192a4f1d3d5326a76862ed95caf8
Seth Gardner <seth.gardner.a@gmail.com>
2021-11-12 14:08:42 -0600

    added navbar to login

--- src/main/java/com/example/basketballgurus/controllers/AuthenticationController.java
+++ src/main/java/com/example/basketballgurus/controllers/AuthenticationController.java
@@ -1,12 +1,22 @@
 package com.example.basketballgurus.controllers;
 
+import com.example.basketballgurus.services.GameBarService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
+
+    private final GameBarService gm;
+
+    public AuthenticationController(GameBarService gm) {
+        this.gm = gm;
+    }
+
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model model) {
+        model.addAttribute("games", gm.getTodaysGames());
         return "user/login";
     }
 }
